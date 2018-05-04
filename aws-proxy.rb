584a22f9583214a34072e40c7f8aa9d4f75730ca
popularsizzle <git@november.fastmail.com>
2018-05-04 10:26:55 +1000

    Add caveat for aws-proxy

--- aws-proxy.rb
+++ aws-proxy.rb
@@ -8,4 +8,33 @@ class AwsProxy < Formula
   def install
     bin.install "aws-proxy"
   end
+
+  def caveats
+    <<~EOS
+      AWS credentials need to be set in ~/.aws/credentials. You can do this by running:
+          aws configure
+
+      Alternatively set the AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables.
+
+      The following is an example IAM policy with the the minimum required permisions:
+          {
+              "Version": "2012-10-17",
+              "Statement": [
+                  {
+                      "Effect": "Allow",
+                      "Action": [
+                          "sts:GetCallerIdentity",
+                          "lightsail:DownloadDefaultKeyPair",
+                          "lightsail:GetBlueprints",
+                          "lightsail:GetBundles",
+                          "lightsail:CreateInstances",
+                          "lightsail:GetInstance",
+                          "lightsail:DeleteInstance"
+                      ],
+                      "Resource": "*"
+                  }
+              ]
+          }
+    EOS
+  end
 end
