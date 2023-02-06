2345b2a3ba896b3053a15c8c537d96578f1773eb
popularsizzle <git@november.fastmail.com>
2023-02-06 18:46:23 +1100

    aws-proxy version bump

--- aws-proxy.rb
+++ aws-proxy.rb
@@ -1,8 +1,8 @@
 class AwsProxy < Formula
   desc "AWS Lightsail SOCKS Proxy"
   homepage "https://github.com/popularsizzle/aws-proxy"
-  url "https://github.com/popularsizzle/aws-proxy/archive/0.9.0.tar.gz"
-  sha256 "834f926165824d92c6a256597b8f195d4a0e84e43cde1c827d395a9b1f0df64f"
+  url "https://github.com/popularsizzle/aws-proxy/archive/0.10.0.tar.gz"
+  sha256 "1a8a9f66e865f992a3e4b585fb5dc0b9dea6eb62e7eee703ffdb7b39cc58be3f"
   depends_on "awscli"
 
   def install
